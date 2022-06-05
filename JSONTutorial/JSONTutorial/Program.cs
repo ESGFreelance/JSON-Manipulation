564a2f020670e21c1495fe33031cab3fc71c0a95
Edward Garcia <edward_12_1998@yahoo.com>
2022-06-05 10:13:13 +0800

    Added Serialization and the Newtonsoft.JSON package

--- JSONTutorial/JSONTutorial/Program.cs
+++ JSONTutorial/JSONTutorial/Program.cs
@@ -1,5 +1,24 @@
-﻿#region Serialization
-//Sample
+﻿using JSONTutorial.Classes;
+using Newtonsoft.Json;
+
+
+#region Default Data
+
+Product P = new Product();
+P.Id = 1;
+P.Name = "Apple";
+P.Quantity = 50;
+
+
+#endregion
+#region Serialization
+string output = JsonConvert.SerializeObject(P);
+JsonSerializer serializer = new JsonSerializer();
+string result = JsonConvert.SerializeObject(P);
+
+
+
+Console.WriteLine();
 #endregion
 
 #region Deserialization
